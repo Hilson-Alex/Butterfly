37bf9ead8c5c04e3906e7ecbf150b677d38d2281
Hilson <hilson.alex234@gmail.com>
2023-03-29 23:30:04 -0300

    Adjust helper

--- src/bfio/program_options.go
+++ src/bfio/program_options.go
@@ -2,6 +2,8 @@ package bfio
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 var OutputPath string
@@ -16,6 +18,13 @@ func init() {
 	flag.StringVar(&OutputPath, "out", "", "Gives a custom path to the generated exe")
 	flag.BoolVar(&ListTokens, "listTokens", false, "List all language tokens and their use. Useful to understand syntax errors")
 	flag.BoolVar(&LexerVerbose, "lexerVerbose", false, "Prints the parsed Tokens while compiling")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: butterfly [flags] [compiling directory]\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Flags for %s:\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "  -help\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "    \tPrints this message\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 	CompileDir = flag.Arg(0)
 }
