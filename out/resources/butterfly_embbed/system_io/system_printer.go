052044b71281f10af45ff8285eb0bb2b35fa30e9
Hilson <hilson.alex234@gmail.com>
2023-05-07 20:39:03 -0300

    hardcoded print module embed on runtime

---
+++ out/resources/butterfly_embbed/system_io/system_printer.go
@@ -0,0 +1,31 @@
+package system_io
+
+import (
+	"fmt"
+
+	"butterfly_embbed/runtime"
+)
+
+func init() {
+	const moduleName = "Standard Output"
+	runtime.BF__EventSubscribe("Sys Println", moduleName, func(message runtime.BF__MessageContent) {
+		switch value := message["text"].(type) {
+		case []interface{}:
+			fmt.Println(value...)
+		default:
+			fmt.Println(value)
+		}
+	})
+	runtime.BF__EventSubscribe("Sys Printf", moduleName, func(message runtime.BF__MessageContent) {
+		var text string
+		var args []any
+		var ok bool
+		if text, ok = message["text"].(string); !ok {
+			panic("Printf message is not a string!")
+		}
+		if args, ok = message["args"].([]interface{}); !ok {
+			panic("Printf args are not an array!")
+		}
+		fmt.Printf(text, args...)
+	})
+}
